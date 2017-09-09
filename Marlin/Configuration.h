cfc2894f90862e50379460d2c867d4b6fcd633c2
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2017-09-09 17:32:35 +0200

    Inverted Z axis because of dual Z mod

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -781,7 +781,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR true     // Change by Akshaal
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true     // Change by Akshaal
 
 // Enable this option for Toshiba stepper drivers
 //#define CONFIG_STEPPERS_TOSHIBA
